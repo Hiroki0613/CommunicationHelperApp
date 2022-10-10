e0e04b24d4850b52b8fa82a0f7df4f1fa1f19fa9
Hiroki Kondo <kondo.matr02@gmail.com>
2022-10-10 13:17:13 +0900

    Initial Commit

---
+++ CommunicationHelperApp/CommunicationHelperAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommunicationHelperAppApp.swift
+//  CommunicationHelperApp
+//
+//  Created by 近藤宏輝 on 2022/10/10.
+//
+
+import SwiftUI
+
+@main
+struct CommunicationHelperAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
