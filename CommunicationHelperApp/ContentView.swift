ebe837dd9f963416a4188d3418caac3c2bb20108
Hiroki Kondo <kondo.matr02@gmail.com>
2022-10-16 00:11:16 +0900

    開発スタート

--- CommunicationHelperApp/ContentView.swift
+++ CommunicationHelperApp/ContentView.swift
@@ -5,6 +5,16 @@
 //  Created by 近藤宏輝 on 2022/10/10.
 //
 
+/*
+ 【詳解】Firebase iOS SDKをSwift Package Manager (SwiftPM)で導入する
+ https://qiita.com/ruwatana/items/80da4b1d5f4906bdd38c
+ https://firebase.google.com/docs/ios/setup?hl=ja
+ https://firebase.google.com/docs/ios/setup
+ 【Xcode】すでに追加済みのSwift Packageの別のパッケージプロダクトを追加する方法
+ https://dev.classmethod.jp/articles/spm-add-new-package-product/
+ 
+ */
+
 import SwiftUI
 
 struct ContentView: View {
