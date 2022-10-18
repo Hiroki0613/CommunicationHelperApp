607292a91a9f1531db75674222b771ebbd2f842a
Hiroki Kondo <kondo.matr02@gmail.com>
2022-10-19 00:14:46 +0900

    コメントを追加

--- CommunicationHelperApp/TopView.swift
+++ CommunicationHelperApp/TopView.swift
@@ -34,6 +34,16 @@ import SwiftUI
 struct TopView: View {
     var body: some View {
         VStack {
+            // TODO: 一番最初の画面でオーナー、スタッフ、作業者を分ける。 初期設定はオーナーが設定するので、少し難し目の作業でもOK
+            
+            // TODO: 2つの画面を用意
+            // TODO: オーナー側はemail、passwordのログイン or appleIDログインを使う。というより、apple側が強制してくる。
+            // TODO: 現時点では、オーナー側はダミーQRコードが出るようにする。
+            // TODO: staff、worker側はカメラが起動して、QRコードリーダーを使えるようにする。
+            // TODO: staff、worker側は正しいQRコードを読み取り次第、アニノマスログインを行う。
+            // TODO: まず、worker側の作成、パルスリーダを実装する
+            // TODO: 設定した時間ごとに画面が変わるようにする。 とりあえず、時間はXcodeで手打ちで設定する。
+            // TODO: ここの段階では心拍数の結果がわかるだけで良い。保存は次のステップにする。
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
