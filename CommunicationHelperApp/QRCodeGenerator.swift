f4260e2b001f8742f8f541f40dd62bb8374259cb
Hiroki Kondo <kondo.matr02@gmail.com>
2022-10-21 22:49:00 +0900

    QRコード生成機能を実装

---
+++ CommunicationHelperApp/QRCodeGenerator.swift
@@ -0,0 +1,26 @@
+//
+//  QRCodeGenerator.swift
+//  CommunicationHelperApp
+//
+//  Created by 近藤宏輝 on 2022/10/21.
+//
+
+import SwiftUI
+
+struct QRCodeGenerator {
+    func generate(with inputText: String) -> UIImage? {
+        guard let qrFilter = CIFilter(name: "CIQRCodeGenerator") else { return nil }
+        
+        let inputData = inputText.data(using: .utf8)
+        qrFilter.setValue(inputData, forKey: "inputMessage")
+        qrFilter.setValue("H", forKey: "inputCorrectionLevel")
+        guard let ciImage = qrFilter.outputImage else { return nil }
+        
+        let sizeTransform = CGAffineTransform(scaleX: 10, y: 10)
+        let scaledCiImage = ciImage.transformed(by: sizeTransform)
+        let context = CIContext()
+        guard let cgImage = context.createCGImage(scaledCiImage, from: scaledCiImage.extent) else { return nil }
+        
+        return UIImage(cgImage: cgImage)
+    }
+}
