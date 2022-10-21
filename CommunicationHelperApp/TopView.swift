3e58c11ccd4311628554c33dc0aa5c2c88f4c5ac
Hiroki Kondo <kondo.matr02@gmail.com>
2022-10-21 22:38:33 +0900

    TopViewのUI実装

--- CommunicationHelperApp/TopView.swift
+++ CommunicationHelperApp/TopView.swift
@@ -13,6 +13,12 @@
  【Xcode】すでに追加済みのSwift Packageの別のパッケージプロダクトを追加する方法
  https://dev.classmethod.jp/articles/spm-add-new-package-product/
  
+ 
+ signInWithApple
+ SwiftUIでFirebaseAuthを使用してSign in with Apple 実装
+ https://qiita.com/hideto1198/items/f0ee7acd757c6ea763b0
+ 
+ 
  */
 
 /*
@@ -33,36 +39,61 @@ import SwiftUI
 
 struct TopView: View {
     var body: some View {
-        VStack {
-            // TODO: 一番最初の画面でオーナー、スタッフ、作業者を分ける。 初期設定はオーナーが設定するので、少し難し目の作業でもOK
-            
-            // TODO: 2つの画面を用意
+        ZStack {
+            Color(red: 0.424, green: 0.780, blue: 0.761)
             VStack {
-                Spacer()
-                Button(
-                    action: {
-                    print("オーナー画面に遷移")
-                }, label: {
-                 Text("Owrner")
-                })
-                Spacer().frame(height: 100)
-                Button(
-                    action: {
-                    print("Staff、Worker画面に遷移")
-                }, label: {
-                 Text("Staff,Worker")
-                })
-                Spacer()
+                // TODO: 一番最初の画面でオーナー、スタッフ、作業者を分ける。 初期設定はオーナーが設定するので、少し難し目の作業でもOK
+                
+                // TODO: 2つの画面を用意
+                
+                    Spacer()
+                    Button(
+                        action: {
+                        print("オーナー画面に遷移")
+                    }, label: {
+                        Text("利用者さん")
+                            .fontWeight(.semibold)
+                             .font(.system(size: 20))
+                             // フォントの色
+                            // ボタンの色
+                             .foregroundColor(Color.black)
+                             .frame(maxWidth: .infinity, minHeight: 96)
+                             .background(Color(red: 0.961, green: 0.961, blue: 0.937))
+
+                             // 両端にpaddingをかける
+                             .padding(.horizontal, 22)
+                    })
+                    Spacer().frame(height: 100)
+                    Button(
+                        action: {
+                        print("Staff、Worker画面に遷移")
+                    }, label: {
+                        Text("支援者さん")
+                            .fontWeight(.semibold)
+                             .font(.system(size: 20))
+                             // フォントの色
+                            // ボタンの色
+                             .foregroundColor(Color.black)
+                             .frame(maxWidth: .infinity, minHeight: 96)
+                             .background(Color(red: 0.961, green: 0.961, blue: 0.937))
+
+                             // 両端にpaddingをかける
+                             .padding(.horizontal, 22)
+                        
+                    })
+                    Spacer()
+                
+                // TODO: オーナー側はemail、passwordのログイン or appleIDログインを使う。というより、apple側が強制してくる。
+                // TODO: 現時点では、オーナー側はダミーQRコードが出るようにする。
+                // TODO: staff、worker側はカメラが起動して、QRコードリーダーを使えるようにする。
+                // TODO: staff、worker側は正しいQRコードを読み取り次第、アニノマスログインを行う。
+                // TODO: まず、worker側の作成、パルスリーダを実装する
+                // TODO: 設定した時間ごとに画面が変わるようにする。 とりあえず、時間はXcodeで手打ちで設定する。
+                // TODO: ここの段階では心拍数の結果がわかるだけで良い。保存は次のステップにする。
+                
             }
-            // TODO: オーナー側はemail、passwordのログイン or appleIDログインを使う。というより、apple側が強制してくる。
-            // TODO: 現時点では、オーナー側はダミーQRコードが出るようにする。
-            // TODO: staff、worker側はカメラが起動して、QRコードリーダーを使えるようにする。
-            // TODO: staff、worker側は正しいQRコードを読み取り次第、アニノマスログインを行う。
-            // TODO: まず、worker側の作成、パルスリーダを実装する
-            // TODO: 設定した時間ごとに画面が変わるようにする。 とりあえず、時間はXcodeで手打ちで設定する。
-            // TODO: ここの段階では心拍数の結果がわかるだけで良い。保存は次のステップにする。
-            
         }
+
     }
 }
 
