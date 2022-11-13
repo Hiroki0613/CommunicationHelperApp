ade6b3a53eadfbec1c1494d0e4490d15d532d35a
Hiroki Kondo <kondo.matr02@gmail.com>
2022-11-13 21:48:49 +0900

    コメントを追加

--- CommunicationHelperApp/View/Worker/WorkerTopView.swift
+++ CommunicationHelperApp/View/Worker/WorkerTopView.swift
@@ -68,6 +68,7 @@ struct WorkerTopView: View {
                     .navigationBarHidden(true)
                 }
             } else {
+                // TODO: ここはif文での分岐ではなく、フルスクリーンカバーにする。そのほうが処理を記載しやすい。
                 WorkerNewSignUpView(
                     store: store,
                     backToTopViewAction: {
