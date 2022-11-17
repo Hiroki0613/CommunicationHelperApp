22f6923b3e78f54425bd658593d4af91fc9ad44e
Hiroki Kondo <kondo.matr02@gmail.com>
2022-11-18 00:27:30 +0900

    Ownerの設定画面を実装開始。

--- CommunicationHelperApp/View/Owner/OwnerManageWorkerView.swift
+++ CommunicationHelperApp/View/Owner/OwnerManageWorkerView.swift
@@ -11,7 +11,39 @@ struct OwnerManageWorkerView: View {
     var body: some View {
         ZStack {
             PrimaryColor.background
-            Text("Hello, World! Worker")
+            ZStack {
+                Rectangle()
+                    .fill(PrimaryColor.buttonColor)
+                    .frame(width: 303, height: 272)
+                    .cornerRadius(20)
+                VStack {
+                    Spacer().frame(height: 18)
+                    HStack {
+                        Spacer().frame(width: 34)
+                        Text("支援者")
+                            .fontWeight(.semibold)
+                            .font(.system(size: 20))
+                            .foregroundColor(Color.black)
+                            .frame(width: 303, height: 30, alignment: .leading)
+                    }
+                    HStack {
+                        Text("開始　０：００")
+                            .fontWeight(.semibold)
+                            .font(.system(size: 20))
+                            .foregroundColor(Color.black)
+                            .frame(width: 303, height: 30, alignment: .trailing)
+                        Spacer().frame(width: 20)
+                    }
+                    HStack {
+                        Text("終了　０：００")
+                            .fontWeight(.semibold)
+                            .font(.system(size: 20))
+                            .foregroundColor(Color.black)
+                            .frame(width: 303, height: 30, alignment: .trailing)
+                        Spacer().frame(width: 20)
+                    }
+                }
+            }
         }
     }
 }
