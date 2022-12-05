c406337caecd6cad66d9cbd4203e54f9dc06003f
Hiroki Kondo <kondo.matr02@gmail.com>
2022-12-05 22:40:20 +0900

    WorkerFiveWsAndOneHChatInputViewを実装

---
+++ CommunicationHelperApp/View/Worker/WorkerFiveWsAndOneHChatInputView.swift
@@ -0,0 +1,20 @@
+//
+//  WorkerFiveWsAndOneHChatInputView.swift
+//  CommunicationHelperApp
+//
+//  Created by 近藤宏輝 on 2022/12/05.
+//
+
+import SwiftUI
+
+struct WorkerFiveWsAndOneHChatInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WorkerFiveWsAndOneHChatInputView_Previews: PreviewProvider {
+    static var previews: some View {
+        WorkerFiveWsAndOneHChatInputView()
+    }
+}
