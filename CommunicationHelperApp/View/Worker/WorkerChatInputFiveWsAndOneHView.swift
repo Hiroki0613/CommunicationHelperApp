69776732c3ea0a1e13957f94c8be277bdea26387
Hiroki Kondo <kondo.matr02@gmail.com>
2022-12-14 00:09:59 +0900

    placeholderをView+extに移動

--- CommunicationHelperApp/View/Worker/WorkerChatInputFiveWsAndOneHView.swift
+++ CommunicationHelperApp/View/Worker/WorkerChatInputFiveWsAndOneHView.swift
@@ -71,16 +71,3 @@ extension String {
         return (self as NSString).size(withAttributes: attributes)
     }
 }
-
-extension View {
-    func placeholder<Content: View>(
-        when shouldShow: Bool,
-        alignment: Alignment = .leading,
-        @ViewBuilder placeholder: () -> Content) -> some View {
-
-        ZStack(alignment: alignment) {
-            placeholder().opacity(shouldShow ? 1 : 0)
-            self
-        }
-    }
-}
