5d08aa40f0a30367357cd884d802a4839a9d39e2
Hiroki Kondo <kondo.matr02@gmail.com>
2023-01-07 22:37:12 +0900

    Owner側のメモを追加

--- CommunicationHelperApp/View/Owner/OwnerTopView.swift
+++ CommunicationHelperApp/View/Owner/OwnerTopView.swift
@@ -15,6 +15,16 @@ struct OwnerTopView: View {
         WithViewStore(store) { viewStore in
             if viewStore.hasRegistrated {
                 TabView {
+                    // TODO: 画面には出さないが、端末管理は無制限に登録させる。作業者のみを課金制にする。
+                    /*
+                     ・端末追加はworker側がQRコードを読み込んで保存。
+                     ・UserDefaultsに情報を保存して、朝礼時にその端末QRコードを表示するようにする。
+                     ・WorkerQRコードはOwner側で生成して、画像 or PDFで読み出しをできるようにする。それを朝礼時に読み込み
+                     ・Firebaseのログイン有無はKeyChainが関わってくるので慎重に行うこと。対策をせずに、迂闊にアプリを消すとクラッシュの原因になる。
+                     ・チャットはOwnerのUUID直下に置くようにする。チャットルームは、独立させる。ただし、Workerとは紐付けをしておいて該当のチャットルームを出すようにする。
+                     ・出勤、不在が一目でわかるようにすると良さそう。
+                     ・オーナー側の作業者の削除機能はどうする？名前を変えて、そのまま使い続けるのも構わない。
+                     */
                     OwnerSettingTopView(viewStore: viewStore)
                         .tabItem {
                             Label("設定", systemImage: "gear")
