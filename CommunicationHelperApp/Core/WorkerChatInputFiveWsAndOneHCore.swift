50a71e84f588eb4b7bf88597326551c618c0b924
Hiroki Kondo <kondo.matr02@gmail.com>
2023-04-25 17:47:51 +0900

    WorkerChatInputFiveWsAndOneHCoreの機能を削除(必要が無かった)

--- CommunicationHelperApp/Core/WorkerChatInputFiveWsAndOneHCore.swift
+++ CommunicationHelperApp/Core/WorkerChatInputFiveWsAndOneHCore.swift
@@ -9,23 +9,23 @@ import ComposableArchitecture
 import Foundation
 
 struct WorkerChatInputFiveWsAndOneHState: Equatable {
-    var allString = ""
-    var hasInputWho = false
-    var hasInputWhat = false
-    var hasInputWhen = false
-    var hasInputWhy = false
-    var hasInputHow = false
-    var showSendTextButton = false
+//    var allString = ""
+//    var hasInputWho = false
+//    var hasInputWhat = false
+//    var hasInputWhen = false
+//    var hasInputWhy = false
+//    var hasInputHow = false
+//    var showSendTextButton = false
 }
 
 enum WorkerChatInputFiveWsAndOneHAction {
-    case afterInputWhere
-    case afterInputWho
-    case afterInputWhat
-    case afterInputWhen
-    case afterInputWhy
-    case afterInputHow
-    case getAllString(whereText: String, whoText: String, whatText: String, whenText: String, whyText: String, howText: String)
+//    case afterInputWhere
+//    case afterInputWho
+//    case afterInputWhat
+//    case afterInputWhen
+//    case afterInputWhy
+//    case afterInputHow
+//    case getAllString(whereText: String, whoText: String, whatText: String, whenText: String, whyText: String, howText: String)
 }
 
 struct WorkerChatInputFiveWsAndOneHEnvironment {
@@ -33,35 +33,34 @@ struct WorkerChatInputFiveWsAndOneHEnvironment {
 
 let workerChatInputFiveWsAndOneHReducer = Reducer<WorkerChatInputFiveWsAndOneHState, WorkerChatInputFiveWsAndOneHAction, WorkerChatInputFiveWsAndOneHEnvironment> { state, action, _ in
     switch action {
-    case .afterInputWhere:
-        state.hasInputWho = true
-        return .none
-
-    case .afterInputWho:
-        state.hasInputWhat = true
-        return .none
-
-    case .afterInputWhat:
-        state.hasInputWhen = true
-        return .none
-
-    case .afterInputWhen:
-        state.hasInputWhy = true
-        return .none
-
-    case .afterInputWhy:
-        state.hasInputHow = true
-        return .none
-
-    case .afterInputHow:
-        // ここで送信ボタンを押せるようにする。
-        return .none
-
-        // TODO: 近藤　ここでチャットの内容を送信している。
-    case let .getAllString(whereText, whoText, whatText, whenText, whyText, howText ):
-        var allString = whereText + "で" + whoText + "が" + whatText + "を" + whenText + "に" + whyText + howText
-        state.allString = allString
-        print("hirohiro_allString: ", state.allString)
-        return .none
+//    case .afterInputWhere:
+//        state.hasInputWho = true
+//        return .none
+//
+//    case .afterInputWho:
+//        state.hasInputWhat = true
+//        return .none
+//
+//    case .afterInputWhat:
+//        state.hasInputWhen = true
+//        return .none
+//
+//    case .afterInputWhen:
+//        state.hasInputWhy = true
+//        return .none
+//
+//    case .afterInputWhy:
+//        state.hasInputHow = true
+//        return .none
+//
+//    case .afterInputHow:
+//        // ここで送信ボタンを押せるようにする。
+//        return .none
+//
+//    case let .getAllString(whereText, whoText, whatText, whenText, whyText, howText ):
+//        var allString = whereText + "で" + whoText + "が" + whatText + "を" + whenText + "に" + whyText + howText
+//        state.allString = allString
+//        print("hirohiro_allString: ", state.allString)
+//        return .none
     }
 }
