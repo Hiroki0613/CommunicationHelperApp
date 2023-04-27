ff1260c6407d39c5aa36824fda2c354d878acf7a
Hiroki Kondo <kondo.matr02@gmail.com>
2023-04-27 12:56:23 +0900

    気圧画面をコメントアウト

--- CommunicationHelperApp/FunctionalParts/AirPressure/AltimatorManager.swift
+++ CommunicationHelperApp/FunctionalParts/AirPressure/AltimatorManager.swift
@@ -1,35 +1,35 @@
+////
+////  AltimatorManager.swift
+////  CommunicationHelperApp
+////
+////  Created by 近藤宏輝 on 2022/11/03.
+////
 //
-//  AltimatorManager.swift
-//  CommunicationHelperApp
+//import Combine
+//import CoreMotion
+//import Foundation
 //
-//  Created by 近藤宏輝 on 2022/11/03.
+//class AltimatorManager: NSObject, ObservableObject {
+//    let willChange = PassthroughSubject<Void, Never>()
+//    var altimeter: CMAltimeter?
+//    @Published var pressureString: String = ""
 //
-
-import Combine
-import CoreMotion
-import Foundation
-
-class AltimatorManager: NSObject, ObservableObject {
-    let willChange = PassthroughSubject<Void, Never>()
-    var altimeter: CMAltimeter?
-    @Published var pressureString: String = ""
-
-    override init() {
-        super.init()
-        altimeter = CMAltimeter()
-        startUpdate()
-    }
-
-    func startUpdate() {
-        if CMAltimeter.isRelativeAltitudeAvailable() {
-            guard let altimeter = altimeter else { return }
-            altimeter.startRelativeAltitudeUpdates(to: OperationQueue.main, withHandler: { data, error in
-                    if error == nil {
-                        let pressure: Double = data!.pressure.doubleValue
-                        self.pressureString = String(format: "気圧:　%.1f hPa", pressure * 10)
-                        self.willChange.send()
-                    }
-            })
-        }
-    }
-}
+//    override init() {
+//        super.init()
+//        altimeter = CMAltimeter()
+//        startUpdate()
+//    }
+//
+//    func startUpdate() {
+//        if CMAltimeter.isRelativeAltitudeAvailable() {
+//            guard let altimeter = altimeter else { return }
+//            altimeter.startRelativeAltitudeUpdates(to: OperationQueue.main, withHandler: { data, error in
+//                    if error == nil {
+//                        let pressure: Double = data!.pressure.doubleValue
+//                        self.pressureString = String(format: "気圧:　%.1f hPa", pressure * 10)
+//                        self.willChange.send()
+//                    }
+//            })
+//        }
+//    }
+//}
--- CommunicationHelperApp/FunctionalParts/AirPressure/PressureView.swift
+++ CommunicationHelperApp/FunctionalParts/AirPressure/PressureView.swift
@@ -1,44 +1,44 @@
+////
+////  PressureView.swift
+////  CommunicationHelperApp
+////
+////  Created by 近藤宏輝 on 2022/11/03.
+////
 //
-//  PressureView.swift
-//  CommunicationHelperApp
+//import ComposableArchitecture
+//import CoreMotion
+//import SwiftUI
 //
-//  Created by 近藤宏輝 on 2022/11/03.
+//struct PressureView: View {
+//    @ObservedObject var manager = AltimatorManager()
+//    let viewStore: ViewStore<OwnerTopState, OwnerTopAction>
+//    let availabe = CMAltimeter.isRelativeAltitudeAvailable()
 //
-
-import ComposableArchitecture
-import CoreMotion
-import SwiftUI
-
-struct PressureView: View {
-    @ObservedObject var manager = AltimatorManager()
-    let viewStore: ViewStore<OwnerTopState, OwnerTopAction>
-    let availabe = CMAltimeter.isRelativeAltitudeAvailable()
-
-    var body: some View {
-        ZStack {
-            PrimaryColor.background
-            VStack(spacing: 30) {
-                VStack(spacing: 30) {
-                    Text(availabe ? manager.pressureString : "----")
-                }
-            }
-        }
-        .onAppear {
-            viewStore.send(.setPressure(manager.pressureString))
-        }
-    }
-}
-
-struct PressureView_Previews: PreviewProvider {
-    static var previews: some View {
-        PressureView(
-            viewStore: ViewStore(
-                Store(
-                    initialState: OwnerTopState(),
-                    reducer: ownerTopReducer,
-                    environment: OwnerTopEnvironment()
-                )
-            )
-        )
-    }
-}
+//    var body: some View {
+//        ZStack {
+//            PrimaryColor.background
+//            VStack(spacing: 30) {
+//                VStack(spacing: 30) {
+//                    Text(availabe ? manager.pressureString : "----")
+//                }
+//            }
+//        }
+//        .onAppear {
+//            viewStore.send(.setPressure(manager.pressureString))
+//        }
+//    }
+//}
+//
+//struct PressureView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        PressureView(
+//            viewStore: ViewStore(
+//                Store(
+//                    initialState: OwnerTopState(),
+//                    reducer: ownerTopReducer,
+//                    environment: OwnerTopEnvironment()
+//                )
+//            )
+//        )
+//    }
+//}
