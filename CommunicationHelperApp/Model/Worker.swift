1720b312cf8307a027385d337b554b9a5564b2f9
Hiroki Kondo <kondo.matr02@gmail.com>
2023-05-10 18:48:22 +0900

    コメントを追加

--- CommunicationHelperApp/Model/Worker.swift
+++ CommunicationHelperApp/Model/Worker.swift
@@ -25,6 +25,7 @@ class WorkerSettingManager: ObservableObject {
     var userDefault: UserDefaultDataStore = UserDefaultsDataStoreProvider.provide()
 
     // TODO: getWorkerDataがリアルタイムで取得できない。
+    // TODO: userDefaults.workerIDと同じ内容をstaffでも用意する。そして、getStaffData()として、WorkerTopViewのonAppearで呼び出す。そうすることで、userDefaultsはどちらかのデータが入ってくる。ただし、この場合はpersonalIdと変更した方が使い勝手が良さそうではある・・・。
     func getWorkerData() {
         guard let ownerId = userDefault.ownerId,
               let deviceId = userDefault.deviceId else { return }
@@ -42,6 +43,15 @@ class WorkerSettingManager: ObservableObject {
                     return nil
                 }
             }
+            // TODO: バグ。deviceIdが重複した場合の処理が入ってない。この処理の前に、workerIdに"worker_"が含まれているかを条件処理に入れることで解決出来そう。いや無理だ・・・。一気にデータを取ったほうがよさそう。
+            /*
+             1. OwnerList-PersonalData-WorkerData
+                OwnerList-PersonalData-StaffData
+                の2つを用意する。
+             2. データを一気に取得する。その上で、どちらを取得しているかを判定する。
+             3. ここで一気にpushTokenをarrayにセット出来たら通信回数を節約できそう。
+                (具体的にどのようにするかは検討が必要)
+             */
             if let index = self.workers.firstIndex(where: { $0.deviceId == deviceId }) {
                 self.userDefault.workerId = self.workers[index].workerId
                 self.userDefault.loginDate = Date()
