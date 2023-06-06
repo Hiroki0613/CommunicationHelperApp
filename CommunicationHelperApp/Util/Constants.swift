2dde14f516ac8d198f2846de34f910ad5eefa9ef
Hiroki Kondo <kondo.matr02@gmail.com>
2023-06-06 22:17:07 +0900

    色の追加

--- CommunicationHelperApp/Util/Constants.swift
+++ CommunicationHelperApp/Util/Constants.swift
@@ -16,6 +16,9 @@ enum PrimaryColor {
     static let buttonLightGray = Color(red: 0.961, green: 0.961, blue: 0.937)
     static let buttonRed = Color(red: 0.758, green: 0.145, blue: 0.145)
     static let backgroundGreen = Color(red: 0.424, green: 0.780, blue: 0.761)
+    static let backgroundGlay = Color(red: 245/255, green: 245/255, blue: 245/255)
+    static let buttonBlue = Color(red: 65/255, green: 105/255, blue: 225/255)
+    static let chatBubbleBackground = Color(red: 250/255, green: 240/255, blue: 230/255)
     // 白黒配色
     static let buttonWhite = Color(red: 0.0, green: 0.0, blue: 0.0)
     static let buttonGray = Color(red: 0.50, green: 0.50, blue: 0.50)
