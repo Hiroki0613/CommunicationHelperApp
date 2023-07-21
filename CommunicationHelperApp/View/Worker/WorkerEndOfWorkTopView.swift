e21f251972b5974bf4117a8087d4578688797663
Hiroki Kondo <kondo.matr02@gmail.com>
2023-07-21 23:19:24 +0900

    メモを追加

--- CommunicationHelperApp/View/Worker/WorkerEndOfWorkTopView.swift
+++ CommunicationHelperApp/View/Worker/WorkerEndOfWorkTopView.swift
@@ -33,7 +33,7 @@ struct WorkerEndOfWorkTopView: View {
                     .padding(.horizontal, 22)
                 Spacer().frame(height: 46)
                 Button(action: {
-                    // TODO: 報告が完了したことを何かしらダイアログで伝えたい。Push通知でも可
+                    // TODO: 報告が完了したことを何かしらダイアログで伝えたい。Push通知でも可。とりあえず、通知できた方が良い。
                     action()
                 }, label: {
                     Text("報告")
