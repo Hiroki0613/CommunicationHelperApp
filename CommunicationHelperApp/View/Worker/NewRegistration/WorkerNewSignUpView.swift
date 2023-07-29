91d9b094e1cd0d639b75a450d19b99acd834a3f1
Hiroki Kondo <kondo.matr02@gmail.com>
2023-07-29 22:24:04 +0900

    メモを追加

--- CommunicationHelperApp/View/Worker/NewRegistration/WorkerNewSignUpView.swift
+++ CommunicationHelperApp/View/Worker/NewRegistration/WorkerNewSignUpView.swift
@@ -38,7 +38,7 @@ struct WorkerNewSignUpView: View {
                     Spacer().frame(height: 80)
                     Button(
                         action: {
-                            // TODO: ここはpush通知の処理が入っているので戻すこと。このままだとカメラ起動画面から戻れなくなってそう。。。
+                            // TODO: ここはpush通知の処理が入っているので戻すこと。このままだとカメラ起動画面から戻れなくなる。
                             // swiftlint:disable line_length
                             pushNotificationSender.sendPushNotification(
                                 to: "",
